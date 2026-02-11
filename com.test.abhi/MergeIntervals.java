469d5152234e1475898b4eb778776c2b8bf5e135
Abhinav-EL <abhinav.yadav138@gmail.com>
2026-02-11 10:29:21 -0500

    More complete implementation of Merge Intervals

--- com.test.abhi/MergeIntervals.java
+++ com.test.abhi/MergeIntervals.java
@@ -1,42 +1,34 @@
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
-//https://leetcode.com/problems/merge-intervals/
+//https://leetcode.com/problems/merge-intervals/ https://neetcode.io/problems/merge-intervals/history
 public class MergeIntervals {
     public int[][] merge(int[][] intervals) {
         
-        Arrays.sort(intervals, (a, b)-> Integer.compare(a[0], b[0]));
+        if(intervals.length<=1){
+            return intervals;
+        }
+        List<int[]> finalResult = new LinkedList<>();
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
 
-        List<int[]> finalList = new ArrayList<>();
-        int[] currInt = intervals[0];
-        if(0 == intervals.length-1){
-            finalList.add(currInt);
-        } else {
-            for(int i=1; i < intervals.length; i++){
-                int[] now = intervals[i];
-                //System.out.println("Passed Now: "+ now[0]+ ","+ now[1]);
-                
-                if(currInt[1]>=now[0]){
-                    //System.out.println("Cond Current Int: "+ currInt[0]+ ","+ currInt[1]);
-                    currInt[1] = Math.max(currInt[1], now[1]);
-                    //System.out.println("Cond Current Int after: "+ currInt[0]+ ","+currInt[1]);
-                } else {
-                    //System.out.println("Current Int Here: "+ currInt[0]+ ","+ currInt[1]);
-                    //System.out.println("Now Int Here: "+ now[0]+ ","+ now[1]);
-                    finalList.add(currInt);
-                    currInt = now;
-                }
-                if(i == intervals.length-1){
-                    finalList.add(currInt);
-                }
+        int[] currInterval = intervals[0];
+        for(int i=1; i<intervals.length; i++){
+            if(intervals[i][0]<=currInterval[1]){
+                currInterval[1] = Math.max(currInterval[1],intervals[i][1]);
+            } else{
+                finalResult.add(currInterval);
+                currInterval = intervals[i];
+            }
+            //Last interval
+            if(i==intervals.length-1){
+                finalResult.add(currInterval);
             }
         }
-        
 
-        int[][] finalArray = new int[finalList.size()][];
-        for(int i=0; i< finalList.size();i++){
-            finalArray[i] = (int[]) finalList.get(i);
+        int[][] finalArray = new int[finalResult.size()][];
+        for(int i=0; i< finalResult.size();i++){
+            finalArray[i] = (int[]) finalResult.get(i);
         }
 
         return finalArray;
